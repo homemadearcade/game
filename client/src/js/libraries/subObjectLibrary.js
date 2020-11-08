4e522b1505ac58fe46133ba5b2fc46fcf3f115e2
Jonathan Pedigo <jon.pedigo@feedly.com>
2020-11-08 14:11:25 -0600

    demo update

--- client/src/js/libraries/subObjectLibrary.js
+++ client/src/js/libraries/subObjectLibrary.js
@@ -3,15 +3,16 @@ window.radianCircle = 6.28319
 window.local.on('onFirstPageGameLoaded', () => {
   window.subObjectLibrary = {
     spear: {
-      // x: 0, y: 0, width: 6, height: 30,
-        x: 0, y: 0, width: 40, height: 40,
+      x: 0, y: 0, width: 6, height: 30,
+        // x: 0, y: 0, width: 40, height: 40,
         relativeX: GAME.grid.nodeSize/5,
         relativeY: -GAME.grid.nodeSize,
         relativeWidth: -GAME.grid.nodeSize * .75,
         relativeHeight: -GAME.grid.nodeSize + 40,
       // opacity: 1,
+      subObjectName: 'spear',
       color: 'yellow',
-      tags: { monsterDestroyer: true, rotateable: true, relativeToAngle: true, relativeToDirection: true },
+      tags: { monsterDestroyer: true, rotateable: true, relativeToAngle: true, relativeToDirection: true, pickupable: true, pickupOnHeroInteract: true, equipOnPickup: true, onMapWhenEquipped: true },
     },
     gun: {
       x: 0, y: 0, width: 10, height: 10,
