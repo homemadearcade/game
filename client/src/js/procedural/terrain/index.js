484634021ab0cd5cb6ceb424930001cb728789d5
Jonathan Pedigo <jon.pedigo@feedly.com>
2020-12-16 01:51:16 -0600

    alrught alright ok so iave improved the map generation, you can even see something that looks like terrain

--- client/src/js/procedural/terrain/index.js
+++ client/src/js/procedural/terrain/index.js
@@ -3,49 +3,95 @@ import { viewNoiseData } from './modals'
 
 import Grid from './Grid'
 
-function generateNoiseMap(width, height, type, seed) {
+window.elevationIntegers = {
+  'Deep Water': 0.2,
+  'Water': 0.4,
+  Sand: 0.5,
+  Grass: 0.7,
+  Forest: 0.8,
+  Mountain: 0.9,
+  Snow: 1
+}
+window.elevationIntegerLookup = {}
+
+for(let i = 0; i <= 100; i+= 1) {
+  const key = (i/100).toFixed(2)
+  if(i <= window.elevationIntegers['Deep Water'] * 100) {
+    window.elevationIntegerLookup[key] = 'Deep Water'
+  } else if(i <= window.elevationIntegers['Water'] * 100) {
+    window.elevationIntegerLookup[key] = 'Water'
+  } else if(i <= window.elevationIntegers['Sand'] * 100) {
+    window.elevationIntegerLookup[key] = 'Sand'
+  } else if(i <= window.elevationIntegers['Grass'] * 100) {
+    window.elevationIntegerLookup[key] = 'Grass'
+  } else if(i <= window.elevationIntegers['Forest'] * 100) {
+    window.elevationIntegerLookup[key] = 'Forest'
+  } else if(i <= window.elevationIntegers['Mountain'] * 100) {
+    window.elevationIntegerLookup[key] = 'Mountain'
+  } else if(i <= window.elevationIntegers['Snow'] * 100) {
+    window.elevationIntegerLookup[key] = 'Snow'
+  }
+}
+
+
+function generateNoiseMap({type, seed, nodes, property, width, height}) {
   if(typeof seed != 'number') seed = Math.random()
 
   var noise = new Noise(seed);
 
+  if(nodes && nodes.length) width = nodes.length
+  if(nodes && nodes[0].length) height = nodes[0].length
+
   if(type == 'perlin') {
-    const perlinGrid = new Grid({startX: 0, startY: 0, width, height})
+    if(!nodes) {
+      const perlinGrid = new Grid({startX: 0, startY: 0, width, height})
+      nodes = perlinGrid.nodes
+    }
 
     for (var x = 0; x < width; x++) {
       for (var y = 0; y < height; y++) {
         // noise.simplex2 and noise.perlin2 return values between -1 and 1.
         var value = noise.perlin2(x / 100, y / 100);
 
-        perlinGrid.nodes[x][y].noise = value
+        // here I convert it to 0-1 and save it
+        nodes[x][y][property] = (value + 1)/2
         //Math.abs(value) * 256; // Or whatever. Open demo.html to see it used with canvas.
       }
     }
-    return perlinGrid.nodes
+    return nodes
   }
 
   if(type == 'simplex') {
-    const simplexGrid = new Grid({startX: 0, startY: 0, width, height})
+    if(!nodes) {
+      const simplexGrid = new Grid({startX: 0, startY: 0, width, height})
+      nodes = simplexGrid.nodes
+    }
 
     for (var x = 0; x < width; x++) {
       for (var y = 0; y < height; y++) {
         // noise.simplex2 and noise.simplex2 return values between -1 and 1.
         var value = noise.simplex2(x / 100, y / 100);
 
-        simplexGrid.nodes[x][y].noise = value
+        // here I convert it to 0-1 and save it
+        nodes[x][y][property] = (value + 1)/2
         //Math.abs(value) * 256; // Or whatever. Open demo.html to see it used with canvas.
       }
     }
-    return simplexGrid.nodes
+    return nodes
   }
 }
 
 async function generateTerrainJSON() {
-  const perlinMap = generateNoiseMap(200, 200, 'perlin', Math.random())
-  await viewNoiseData(perlinMap, 'perlin')
+  const grid = new Grid({startX: 0, startY: 0, width: 300, height: 300})
 
-  const simplexMap = generateNoiseMap(200, 200, 'simplex', Math.random())
-  await viewNoiseData(simplexMap, 'simplex')
+  generateNoiseMap({type: 'perlin', seed: Math.random(), nodes: grid.nodes, property: 'moisture'})
+  await viewNoiseData({noiseNodes: grid.nodes, nodeProperty: 'moisture', title: 'perlin', type: 'terrain'})
+
+  generateNoiseMap({type: 'simplex', seed: Math.random(), nodes: grid.nodes, property: 'height'})
+  await viewNoiseData({noiseNodes: grid.nodes, nodeProperty: 'height', title: 'simplex', type: 'terrain'})
 }
+
+
 
 export {
   generateTerrainJSON
--- client/src/js/procedural/terrain/modals.js
+++ client/src/js/procedural/terrain/modals.js
@@ -2,7 +2,22 @@ import * as PIXI from 'pixi.js'
 import Swal from 'sweetalert2/src/sweetalert2.js';
 import tinycolor from 'tinycolor2';
 
-function viewNoiseData(noiseNodes, title) {
+window.elevationColors = {
+  'Deep Water': parseInt(tinycolor('#00008b').toHex(), 16),
+  'Water': parseInt(tinycolor('#4F42B5').toHex(), 16),
+  Sand: parseInt(tinycolor('#ffe29c').toHex(), 16),
+  Grass: parseInt(tinycolor('#567d46').toHex(), 16),
+  Forest: parseInt(tinycolor('#1F3D0C').toHex(), 16),
+  Mountain: parseInt(tinycolor('#5b5036').toHex(), 16),
+  Snow: parseInt(tinycolor('#eee').toHex(), 16)
+}
+
+
+function viewNoiseData({noiseNodes, nodeProperty, title, type}) {
+
+  const colorData = Object.keys(window.elevationColors).map((name) => {
+
+  })
   const promise = Swal.fire({
     title,
     showClass: {
@@ -13,6 +28,9 @@ function viewNoiseData(noiseNodes, title) {
     },
     html:"<canvas id='noise-viewer'></canvas>",
     width: '840px'
+  }).then(() => {
+
+    app.stage.removeChildren()
   })
 
   const app = new PIXI.Application({
@@ -20,11 +38,23 @@ function viewNoiseData(noiseNodes, title) {
   });
 
   noiseNodes.forEach((row, x) => {
-    row.forEach(({noise}, y) => {
+    row.forEach((node, y) => {
       const sprite = new PIXI.Sprite(PIXI.Texture.WHITE)
-      sprite.tint = 0xFFFFFF
       // console.log(noise, (noise + 1)/2)
-      sprite.alpha = (noise + 1)/2
+
+      const prop = node[nodeProperty].toFixed(2)
+      if(type == 'terrain') {
+        const terrainType = window.elevationIntegerLookup[prop]
+        sprite.tint = window.elevationColors[terrainType]
+        if(!terrainType) console.log(prop)
+      } else if(type == 'water'){
+        if (prop < 0.4) sprite.tint = 0x0000FF;
+        else sprite.tint = 0xFFFFFF
+      } else {
+        sprite.tint = 0xFFFFFF
+        sprite.alpha = prop
+      }
+
       sprite.x = x
       sprite.y = y
       sprite.width = 1
